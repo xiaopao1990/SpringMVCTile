b75101efe099865b3d6fde16cd0bf6b10714284b
xiaopao1990 <shawnjiaowork@gmail.com>
2015-01-28 16:36:48 -0500

    SpringMVC with Tile(outlay tools)

---
+++ springTail/src/com/javatpoint/controller/ContactController.java
@@ -0,0 +1,23 @@
+package com.javatpoint.controller;  
+import org.springframework.stereotype.Controller;  
+import org.springframework.validation.BindingResult;  
+import org.springframework.web.bind.annotation.ModelAttribute;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMethod;  
+import org.springframework.web.bind.annotation.SessionAttributes;  
+import org.springframework.web.servlet.ModelAndView;  
+import com.javatpoint.form.Contact;  
+@Controller  
+@SessionAttributes  
+public class ContactController {  
+    @RequestMapping(value = "/addContact", method = RequestMethod.POST)  
+    public String addContact(@ModelAttribute("contact") Contact contact, BindingResult result) {  
+        //write the code here to add contact  
+        return "redirect:contact.html";  
+    }  
+      
+    @RequestMapping("/contact")  
+    public ModelAndView showContacts() {  
+        return new ModelAndView("contact", "command", new Contact());  
+    }  
+}  
---
+++ springTail/src/com/javatpoint/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.javatpoint.controller;  
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.servlet.ModelAndView;  
+@Controller  
+public class HelloController {  
+    @RequestMapping("/hello")  
+    public ModelAndView helloWorld() {  
+        String message = "Hello World, Spring MVC @ Javatpoint";  
+        return new ModelAndView("hello", "message", message);  
+    }  
+}  
